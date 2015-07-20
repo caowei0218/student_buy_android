9dc194ee4ec16199d6d1df2bfe6cb5c69bddd8e9
曹溦 <caowei0218@qq.com>
2015-07-20 21:19:19 +0800

    消息josn处理

---
+++ src/com/example/student_buy_android/bean/Message.java
@@ -0,0 +1,21 @@
+package com.example.student_buy_android.bean;
+
+public class Message {
+	
+	private String msg;
+	private String uid;
+	
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	
+}
